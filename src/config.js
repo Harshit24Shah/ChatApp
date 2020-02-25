5763bda00c4ff810dda6561a5a39c85e915f3fa3
Harshit24Shah <harshitshah24@gmail.com>
2020-02-25 20:46:16 +0530

    Create config.js
    config file created

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const instanceLocator = 'v1:us1:72c38e29-d840-4eeb-b0c2-6991188a42e0';
+
+const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/72c38e29-d840-4eeb-b0c2-6991188a42e0/token';
+
+export { instanceLocator, tokenUrl }
